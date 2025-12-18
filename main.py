0a1176d7fc466d7945d68ed05d88429f02b48252
bezdarnij <paschabmw@mail.ru>
2025-12-18 23:13:11 +0300

    player

--- main.py
+++ main.py
@@ -20,10 +20,15 @@ class Player(arcade.Sprite):
         self.center_y = SCREEN_HEIGHT // 2
         self.change_x = 0
         self.change_y = 0
-        self.texture_left = arcade.load_texture("images/player.png")
-        self.texture_right = arcade.load_texture("images/player.png").flip_left_right()
-        self.astral_texture_left = arcade.load_texture("images/player_astral.png")
-        self.astral_texture_right = arcade.load_texture("images/player_astral.png").flip_left_right()
+        self.texture = arcade.load_texture("images/player_back.png")
+        self.texture_left = arcade.load_texture("images/player_left.png").flip_left_right()
+        self.texture_right = arcade.load_texture("images/player_left.png")
+        self.texture_back = arcade.load_texture("images/player_back.png")
+        self.texture_forward = arcade.load_texture("images/player.png")
+        self.astral_texture_left = arcade.load_texture("images/player_astral_left.png").flip_left_right()
+        self.astral_texture_right = arcade.load_texture("images/player_astral_left.png")
+        self.astral_texture_back = arcade.load_texture("images/player_astral_back.png")
+        self.astral_texture_forward = arcade.load_texture("images/player_astral.png")
         self.astral_form = False
         self.astral_form_x = 0
         self.astral_form_y = 0
@@ -48,7 +53,8 @@ class Astral_Escape(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         self.background = arcade.load_texture("images/prison.png")
-        self.track = True
+        self.track_h = 2
+        self.track_v = 2
         # устройства
         self.devices = None
         self.current_device = None
@@ -66,7 +72,6 @@ class Astral_Escape(arcade.Window):
         camera = Camera(300, 400)
         self.devices.append(camera)
         self.change_form = arcade.SpriteList(self.player.texture_left)
-        print(self.player.size, self.player.width)
 
     def on_draw(self):
         self.clear()
@@ -76,7 +81,7 @@ class Astral_Escape(arcade.Window):
                                                   SCREEN_WIDTH, SCREEN_HEIGHT)
                                  )
         if self.player.astral_form:
-            arcade.draw_texture_rect(self.player.texture_left,
+            arcade.draw_texture_rect(self.player.texture_right,
                                      arcade.rect.XYWH(self.player.astral_form_x, self.player.astral_form_y, 100,
                                                       100))
         if self.current_device:
@@ -88,15 +93,26 @@ class Astral_Escape(arcade.Window):
 
     def on_update(self, delta_time):
         if not self.player.astral_form:
-            if self.track:
+            if self.track_h == 1:
                 self.player.texture = self.player.texture_left
-            else:
+            if self.track_h == 0:
                 self.player.texture = self.player.texture_right
+            if self.track_v == 1:
+                self.player.texture = self.player.texture_back
+            if self.track_v == 0:
+                self.player.texture = self.player.texture_forward
         else:
-            if self.track:
+            if self.track_h == 1:
                 self.player.texture = self.player.astral_texture_left
-            else:
+            if self.track_h == 0:
                 self.player.texture = self.player.astral_texture_right
+            if self.track_v == 1:
+                self.player.texture = self.player.astral_texture_back
+            if self.track_v == 0:
+                self.player.texture = self.player.astral_texture_forward
+        if self.player.astral_form and self.track_h == 2 and self.track_v == 2:
+            pass
+
         self.player.update(delta_time)
 
         self.current_device = None
@@ -110,24 +126,28 @@ class Astral_Escape(arcade.Window):
     def on_key_press(self, key, modifiers):
         if key == arcade.key.W:
             self.player.change_y = 1
+            self.track_v = 1
         elif key == arcade.key.S:
             self.player.change_y = -1
+            self.track_v = 0
         elif key == arcade.key.A:
             self.player.change_x = -1
-            self.track = True
+            self.track_h = 1
         elif key == arcade.key.D:
             self.player.change_x = 1
-            self.track = False
+            self.track_h = 0
         elif key == arcade.key.Q:
             if self.player.astral_form:
                 self.player.astral_form = False
                 self.player.center_x = self.player.astral_form_x
                 self.player.center_y = self.player.astral_form_y
+                self.player.texture = self.player.texture_forward
             else:
                 self.player.astral_form = True
                 self.player.astral_form_x = self.player.center_x
                 self.player.astral_form_y = self.player.center_y
                 self.player.center_x = self.player.center_x + self.player.width
+                self.player.texture = self.player.astral_texture_forward
         elif key == arcade.key.E:
             if self.current_device and not self.current_device.is_hacked:
                 self.current_device.hack()
@@ -135,8 +155,10 @@ class Astral_Escape(arcade.Window):
     def on_key_release(self, key, modifiers):
         if key == arcade.key.W or key == arcade.key.S:
             self.player.change_y = 0
+            self.track_v = 2
         elif key == arcade.key.A or key == arcade.key.D:
             self.player.change_x = 0
+            self.track_h = 2
 
 
 def setup_game(width=800, height=600, title="Red Hat collects berries"):
